469d5d9186814d0b99d330c0add4669ffcea833e
Joe <salutegx@gmail.com>
2025-01-16 13:08:10 +0000

    Update Exploratory analysis with text mining.R

--- Exploratory analysis with text mining.R
+++ Exploratory analysis with text mining.R
@@ -1,9 +1,12 @@
+#Install and acsess the required packages
 install.packages("wordcloud")
 install.packages("tm")        
 install.packages("RColorBrewer")
 library(wordcloud)
 library(tm)
 library(RColorBrewer)
+
+
 #Exploratory analysis with text mining
 
 #load data
@@ -12,14 +15,14 @@ class(newsdata$content)
 data <- na.omit(data)
 sport_news <- filter(data, category_level_1 == "sport")
 sport_news_docs <-Corpus(VectorSource(sport_news$content))
-wordcloud <- c(stopwords("en"), "will", "also", "thus", "however", "like", "one", "just", "get", "can", "'s", "said", "much", "make", "made","now","will",)
+wordcloud <- c(stopwords("en"), "will", "also", "thus", "however", "like", "one", "just", "get", "can", "'s", "said", "much", "make", "made","now","will")
 sport_news_docs <- tm_map(sport_news_docs, tolower)
 sport_news_docs <- tm_map(sport_news_docs, removePunctuation)
 sport_news_docs <- tm_map(sport_news_docs, removeNumbers)
 sport_news_docs <- tm_map(sport_news_docs, removeWords, wordcloud)
 sport_news_docs <- tm_map(sport_news_docs, stripWhitespace)
 
-#frequency and correlation
+#Frequency and correlation
 sportnews_dtm <- DocumentTermMatrix(sport_news_docs)
 findFreqTerms(sportnews_dtm, lowfreq=100)
 findAssocs(sportnews_dtm, 'football',
@@ -38,11 +41,14 @@ wordcloud_dtm_matrix <- as.matrix(wordcloud_dtm)
 wordcloud_dtm_freq <- sort(rowSums(wordcloud_dtm_matrix), decreasing = TRUE)
 wordcloud_data <- data.frame(word = names(wordcloud_dtm_freq), freq = wordcloud_dtm_freq)
 set.seed(1234)
-wordcloud(words = wordcloud_data$word, freq = wordcloud_data$freq, 
+
+# Different word cloud styles
+#wordcloud(words = wordcloud_data$word, freq = wordcloud_data$freq, 
           min.freq = 50,      
           max.words = 100,  
           random.order = FALSE,
           colors = brewer.pal(8, "Dark2"))
+
 wordcloud(words = wordcloud_data$word, freq = wordcloud_data$freq,
           min.freq = 2, scale = c(3, 0.5), colors = brewer.pal(6, "Set2"),
           random.order = FALSE, rot.per = 0.35, use.r.layout = FALSE)
